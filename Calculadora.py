ead0cda71723aa020e0aceea3e2aba32cd302767
IvanFerreira06 <149527894+IvanFerreira06@users.noreply.github.com>
2023-10-31 21:00:49 +0000

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -2,6 +2,8 @@ conta = str(input("Introduz a conta (2 + 3): "));
 
 conta_divida = conta.split(" ");
 
+historico = [];
+
 match conta_divida[1]:
     case "+":
         resultado = float(conta_divida[0]) + float(conta_divida[2]);
@@ -13,3 +15,8 @@ match conta_divida[1]:
         resultado = float(conta_divida[0]) / float(conta_divida[2]);
 
 print(f"{conta} = {resultado}");
+historico.append(str(conta) + " = " + str(resultado));
+
+op = str(input("Introduz H se quiser ver o histórico: "));
+if op == "H" or op == "h":
+    print(historico);
